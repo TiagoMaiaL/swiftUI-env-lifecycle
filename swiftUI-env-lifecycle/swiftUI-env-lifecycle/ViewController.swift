74483c8248a422d20cd1808e89ea2befed771a7c
TiagoMaiaL <tiago.maia.lopes@gmail.com>
2022-10-27 20:05:24 -0300

    Add initial project

---
+++ swiftUI-env-lifecycle/swiftUI-env-lifecycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftUI-env-lifecycle
+//
+//  Created by Knowledge Temple on 27/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
